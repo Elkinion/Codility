06ad918135625447338a8f36af1521dd558284d3
elkinion <elkinionx@gmail.com>
2022-09-10 22:13:28 -0500

    comentarios primer problema

--- codility 1.py
+++ codility 1.py
@@ -6,10 +6,12 @@ Created on Wed Sep  7 16:50:13 2022
 """
 
 def solution(n):
-    result=0
-    count=0
+    #here we divide by 2 until there are not 0s at the right oif the binary representation of n
     while n%2==0:
         n=n//2
+    result=0
+    #for each strike we will count how many 0s we have
+    count=0
     while n>1:
         if n%2==1:
             n=n//2
@@ -17,8 +19,8 @@ def solution(n):
         else:
             n=n//2
             count+=1
+        #we want te max count strike of 0s 
         result=max(result,count)
     
     return(result)
-
     pass

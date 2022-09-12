bc96a99c88ffca881f7c70ffa0a97577816e735a
elkinion <elkinionx@gmail.com>
2022-09-11 21:01:27 -0500

    lesson 2 but diferent

--- codility_2_OddOccurrencesInArray.py
+++ codility_2_OddOccurrencesInArray.py
@@ -8,6 +8,17 @@ Created on Sun Sep 11 20:35:14 2022
 def solution(A):
     n=0
     for i in A:
-        n=n^i
+        n=n^i#Xor operator is all the trick, a^a=0 and the operation is abelian.
     return(n)
     pass
+
+
+#another way to do it.
+from functools import reduce
+
+def sol(B):
+    r = reduce((lambda x, y: x ^ y), B)
+    return(r)
+
+#list1 = [1, 2, 3,2,1]
+#sol(list1)
